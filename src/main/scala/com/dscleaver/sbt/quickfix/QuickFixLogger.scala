601963b4e6c5e81c0a4fbd108396ffb643096715
Aldo Stracquadanio <aldo.strac@gmail.com>
2014-06-20 12:46:29 +0100

    Auto-truncate quickfix file on clean build
    (cherry picked from commit 1f170262d3882f9d07a9b1e1ab82ed9b6f8ff069)

--- src/main/scala/com/dscleaver/sbt/quickfix/QuickFixLogger.scala
+++ src/main/scala/com/dscleaver/sbt/quickfix/QuickFixLogger.scala
@@ -3,10 +3,10 @@ package com.dscleaver.sbt.quickfix
 import sbt._
 
 object QuickFixLogger {
-  def append(output: File, prefix: String, message: String): Unit = 
+  def append(output: File, prefix: String, message: String): Unit =
     IO.append(output, "[%s] %s\n".format(prefix, message))
 
-  def append(output: File, prefix: String, file: File, line: Int, message: String): Unit = 
+  def append(output: File, prefix: String, file: File, line: Int, message: String): Unit =
     append(output, prefix, "%s:%d: %s".format(file, line, message))
 }
 
@@ -21,17 +21,18 @@ class QuickFixLogger(val output: File, vimExec: String, enableServer: Boolean) e
     case _ => handleDebugMessage(message)
   }
 
-  def handleDebugMessage(message: String) =
+  def handleDebugMessage(message: String) = {
+    if (message.toLowerCase.contains("initial source changes:")) {
+      IO.delete(output)
+      IO.touch(List(output))
+    }
+
     if (enableServer && message.toLowerCase.contains("compilation failed")) {
       call(vimExec, "<esc>:cfile %s<cr>".format(output.toString))
     }
-
-  def handleInfoMessage(message: String) = {
-    if(message startsWith "Compiling") {
-      IO.delete(output)
-      IO.touch(List(output))
-    } else ()
   }
+
+  def handleInfoMessage(message: String) = ()
 
   def handleErrorMessage(message: String) = append(output, "error", message)
 
